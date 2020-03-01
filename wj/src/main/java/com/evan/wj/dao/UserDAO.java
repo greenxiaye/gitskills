ba7e93ec128672289b2537855ba756811483dcc9
greenxiaye <greenxiaye@163.com>
2020-03-02 03:57:11 +0800

    init

---
+++ wj/src/main/java/com/evan/wj/dao/UserDAO.java
@@ -0,0 +1,15 @@
+package com.evan.wj.dao;
+
+import com.evan.wj.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDAO extends JpaRepository<User,Integer> {
+    User findByUsername(String username);
+
+    User getByUsernameAndPassword(String username,String password);
+}
+
+//package com.evan.wj.dao;
+//
+//public interface UserDAO {
+//}
---
+++ wj/src/main/java/com/evan/wj/pojo/User.java
@@ -0,0 +1,79 @@
+package com.evan.wj.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@JsonIgnoreProperties({"handler","hibernateLazyInitializer"})
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    String username;
+    String password;
+    public User(){
+
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
+
+
+//package com.evan.wj.pojo;
+//
+//public class User {
+//    int id;
+//    String username;
+//    String password;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//}
