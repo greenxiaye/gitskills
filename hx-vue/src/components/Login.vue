7ef9ae72cbff67c1f8f419e6f1ef8943cf834bb1
greenxiaye <greenxiaye@163.com>
2020-03-02 14:26:15 +0800

    test1.1

--- hx-vue/src/components/Login.vue
+++ hx-vue/src/components/Login.vue
@@ -56,6 +56,8 @@ export default {
   },
   methods: {
     login () {
+      var _this = this
+      console.log(this.$store.state)
       this.$axios
         .post('/login', {
           username: this.loginForm.username,
@@ -63,11 +65,28 @@ export default {
         })
         .then(successResponse => {
           if (successResponse.data.code === 200) {
-            this.$router.replace({ path: '/index' })
+            // var data = this.loginForm
+            _this.$store.commit('login', _this.loginForm)
+            var path = this.$route.query.redirect
+            this.$router.replace({path: path === '/' || path === undefined ? '/index' : path})
           }
         })
-        .catch(failResponse => {})
+        .catch(failResponse => {
+        })
     }
+    // login () {
+    //   this.$axios
+    //     .post('/login', {
+    //       username: this.loginForm.username,
+    //       password: this.loginForm.password
+    //     })
+    //     .then(successResponse => {
+    //       if (successResponse.data.code === 200) {
+    //         this.$router.replace({ path: '/index' })
+    //       }
+    //     })
+    //     .catch(failResponse => {})
+    // }
   }
 }
 </script>
